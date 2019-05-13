8c863acfd5c5331deb4a2ec38c06fe80c2c2ca6d
jessica McCreery <jessica.mccreery@ibm.com>
2019-05-13 15:04:38 +0100

    import path

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { combineReducers } from '../../../../../../../Library/Caches/typescript/2.9/node_modules/redux';
+import { combineReducers } from 'redux';
 
 import extensions, * as extensionSelectors from './extensions';
 import namespaces, * as namespaceSelectors from './namespaces';
